545b56731c3374e3cf8e88043c819da47b32f04a
Jake McLeman <jake@mcleman.com>
2023-01-06 17:17:41 -0800

    set up pixel rendering:

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,40 @@
-// Bevy code commonly triggers these lints and they may be important signals
-// about code quality. They are sometimes hard to avoid though, and the CI
-// workflow treats them as errors, so this allows them throughout the project.
-// Feel free to delete this line.
-#![allow(clippy::too_many_arguments, clippy::type_complexity)]
-
 use bevy::prelude::*;
+use bevy_pixel_camera::{
+    PixelBorderPlugin, PixelCameraBundle, PixelCameraPlugin
+};
+
+mod sprite_anim;
+use sprite_anim::SpriteAnimator;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .insert_resource(ClearColor(Color::BLACK))
+        .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
+        .add_plugin(PixelCameraPlugin)
+        .add_plugin(PixelBorderPlugin {
+            color: Color::BLACK,
+        })
         .add_startup_system(setup)
+        .add_system(sprite_anim::animate_sprite)
         .run();
 }
 
-fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
-    commands.spawn(Camera2dBundle::default());
-    commands.spawn(SpriteBundle {
-        texture: asset_server.load("icon.png"),
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>, mut texture_atlases: ResMut<Assets<TextureAtlas>>,) {
+    // commands.spawn(Camera2dBundle {
+    //     camera_2d: Camera2d {
+    //         clear_color: ClearColorConfig::Custom(Color::BLACK),
+    //     },
+    //     ..Default::default()
+    // });
+    
+    commands.spawn(PixelCameraBundle::from_resolution(160, 120));
+
+    let texture_handle =  asset_server.load("sam1.png");
+    let texture_atlas = TextureAtlas::from_grid(texture_handle, Vec2::new(42., 32.), 4, 1, None, None);
+    let texture_atlas_handle = texture_atlases.add(texture_atlas);
+    
+    commands.spawn(SpriteSheetBundle {
+        texture_atlas: texture_atlas_handle.clone(),
         ..Default::default()
-    });
+    }).insert(SpriteAnimator::new(texture_atlas_handle.clone(), 0, 3, 4, 0.2, true));
 }
---
+++ src/sprite_anim.rs
@@ -0,0 +1,85 @@
+use bevy::prelude::*;
+
+#[derive(Component)]
+pub struct SpriteAnimator {
+    _atlas_handle: Handle<TextureAtlas>,
+    start_frame: usize,
+    end_frame: usize,
+    row_length: usize,
+    seconds_per_frame: f32,
+    frame_timer: f32,
+    pub should_loop: bool,
+    playing: bool,
+    restart_anim: bool,
+}
+
+impl SpriteAnimator {
+    pub fn new(
+        _atlas_handle: Handle<TextureAtlas>,
+        start_frame: usize,
+        end_frame: usize,
+        row_length: usize,
+        seconds_per_frame: f32,
+        should_loop: bool,
+    ) -> SpriteAnimator {
+        SpriteAnimator {
+            _atlas_handle,
+            start_frame,
+            end_frame,
+            row_length,
+            seconds_per_frame,
+            frame_timer: 0.,
+            should_loop,
+            playing: true,
+            restart_anim: false,
+        }
+    }
+
+    pub fn _play(&mut self) {
+        self.playing = true;
+    }
+
+    pub fn _pause(&mut self) {
+        self.playing = false;
+    }
+
+    pub fn set_row(&mut self, row_index: usize) {
+        self.start_frame = row_index * self.row_length;
+        self.end_frame = self.start_frame + self.row_length - 1;
+        self.restart_anim = true;
+    }
+}
+
+pub fn animate_sprite(
+    time: Res<Time>,
+    mut sprites: Query<(
+        &mut SpriteAnimator, 
+        &mut TextureAtlasSprite,
+    )>
+) {
+    for (mut animator, mut sprite) in sprites.iter_mut() {
+        if animator.playing {
+            animator.frame_timer += time.delta_seconds();
+
+            if animator.restart_anim || animator.frame_timer > animator.seconds_per_frame {
+                animator.frame_timer = 0.;
+
+                let mut next_index = sprite.index + 1;
+
+                if animator.restart_anim || next_index > animator.end_frame {
+                    next_index = animator.start_frame;
+                    animator.restart_anim = false;
+                    
+                }
+
+                if !animator.should_loop && next_index == animator.end_frame {
+                    animator.playing = false;
+                    animator.restart_anim = false;
+                }
+
+                sprite.index = next_index;
+            }
+        }
+        
+    }
+}
