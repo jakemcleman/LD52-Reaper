b63082caee98a5244454920074b3288c20087582
Jake McLeman <jake@mcleman.com>
2023-01-06 20:12:25 -0800

    switched back to ortho cam

--- src/camera.rs
+++ src/camera.rs
@@ -2,11 +2,7 @@ use bevy::prelude::*;
 use bevy_ecs_ldtk::{LdtkLevel, LevelSelection};
 use crate::ASPECT_RATIO;
 use crate::player::Player;
-use bevy_pixel_camera::{PixelCameraBundle, PixelProjection};
-use crate::{GameState, VIEW_WIDTH, VIEW_HEIGHT};
-use bevy_pixel_camera::{
-    PixelBorderPlugin, PixelCameraPlugin
-};
+use crate::GameState;
 
 pub struct CameraPlugin;
 
@@ -14,10 +10,6 @@ impl Plugin for CameraPlugin {
     fn build(&self, app: &mut App) {
         app
             .insert_resource(ClearColor(Color::BLACK))
-            .add_plugin(PixelCameraPlugin)
-            .add_plugin(PixelBorderPlugin {
-                color: Color::BLACK,
-            })
             .add_startup_system(spawn_camera)
             .add_system_set(SystemSet::on_update(GameState::Playing).with_system(camera_fit_inside_current_level))
         ;
@@ -25,13 +17,15 @@ impl Plugin for CameraPlugin {
 }
 
 fn spawn_camera(mut commands: Commands) {
-    commands.spawn(PixelCameraBundle::from_resolution(VIEW_WIDTH, VIEW_HEIGHT));
+    //commands.spawn(PixelCameraBundle::from_resolution(VIEW_WIDTH, VIEW_HEIGHT));
+    let camera = Camera2dBundle::default();
+    commands.spawn(camera);
 }
 
-fn camera_fit_inside_current_level(
+pub fn camera_fit_inside_current_level(
     mut camera_query: Query<
         (
-            &mut PixelProjection,
+            &mut bevy::render::camera::OrthographicProjection,
             &mut Transform,
         ),
         Without<Player>,
@@ -39,9 +33,9 @@ fn camera_fit_inside_current_level(
     player_query: Query<&Transform, With<Player>>,
     level_query: Query<
         (&Transform, &Handle<LdtkLevel>),
-        (Without<PixelProjection>, Without<Player>),
+        (Without<OrthographicProjection>, Without<Player>),
     >,
-    _level_selection: Res<LevelSelection>,
+    level_selection: Res<LevelSelection>,
     ldtk_levels: Res<Assets<LdtkLevel>>,
 ) {
     if let Ok(Transform {
@@ -49,17 +43,42 @@ fn camera_fit_inside_current_level(
         ..
     }) = player_query.get_single()
     {
-        let _player_translation = *player_translation;
+        let player_translation = *player_translation;
 
-        let (mut _projection, mut camera_transform) = camera_query.single_mut();
+        let (mut orthographic_projection, mut camera_transform) = camera_query.single_mut();
 
         for (level_transform, level_handle) in &level_query {
             if let Some(ldtk_level) = ldtk_levels.get(level_handle) {
                 let level = &ldtk_level.level;
-                camera_transform.translation.x = level_transform.translation.x + (level.px_wid as f32) / 2.;
-                camera_transform.translation.y = level_transform.translation.y + (level.px_hei as f32) / 2.;
-                
-                
+                if level_selection.is_match(&0, level) {
+                    let level_ratio = level.px_wid as f32 / ldtk_level.level.px_hei as f32;
+
+                    orthographic_projection.scaling_mode = bevy::render::camera::ScalingMode::None;
+                    orthographic_projection.bottom = 0.;
+                    orthographic_projection.left = 0.;
+                    if level_ratio > ASPECT_RATIO {
+                        // level is wider than the screen
+                        orthographic_projection.top = (level.px_hei as f32 / 9.).round() * 9.;
+                        orthographic_projection.right = orthographic_projection.top * ASPECT_RATIO;
+                        camera_transform.translation.x = (player_translation.x
+                            - level_transform.translation.x
+                            - orthographic_projection.right / 2.)
+                            .clamp(0., level.px_wid as f32 - orthographic_projection.right);
+                        camera_transform.translation.y = 0.;
+                    } else {
+                        // level is taller than the screen
+                        orthographic_projection.right = (level.px_wid as f32 / 16.).round() * 16.;
+                        orthographic_projection.top = orthographic_projection.right / ASPECT_RATIO;
+                        camera_transform.translation.y = (player_translation.y
+                            - level_transform.translation.y
+                            - orthographic_projection.top / 2.)
+                            .clamp(0., level.px_hei as f32 - orthographic_projection.top);
+                        camera_transform.translation.x = 0.;
+                    }
+
+                    camera_transform.translation.x += level_transform.translation.x;
+                    camera_transform.translation.y += level_transform.translation.y;
+                }
             }
         }
     }
